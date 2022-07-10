6380c25058d0dfbd34024a7f81e7ac56267f2c08
Hayel <ivanforwork@yandex.ru>
2022-07-11 02:35:05 +0300

    Applying an indent solved.

---
+++ src/main/java/field/string/ApplyingIndent.java
@@ -0,0 +1,8 @@
+package field.string;
+
+public class ApplyingIndent {
+  // JDK 12+ only
+  public static String indent(String text, int indent) {
+    return text.indent(indent);
+  }
+}
---
+++ src/test/java/field/string/ApplyingIndentTest.java
@@ -0,0 +1,21 @@
+package field.string;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ApplyingIndentTest {
+  private static final String DAYS =
+      "Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday";
+  private static final String EXPECTED_DAYS =
+      "  Monday\n  Tuesday\n  Wednesday\n  Thursday\n  Friday\n  Saturday\n  Sunday\n";
+
+  /**
+   * Method under test: {@link ApplyingIndent#indent(String, int)}
+   */
+  @Test
+  void testIndent() {
+    assertEquals(" Text\n", ApplyingIndent.indent("Text", 1));
+    assertEquals(EXPECTED_DAYS, ApplyingIndent.indent(DAYS, 2));
+  }
+}
