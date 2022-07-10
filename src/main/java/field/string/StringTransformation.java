b7722040806c4a0f8a8a92192c98e92f1f707254
Hayel <ivanforwork@yandex.ru>
2022-07-11 02:47:52 +0300

    String transformation solved.

---
+++ src/main/java/field/string/StringTransformation.java
@@ -0,0 +1,15 @@
+package field.string;
+
+import java.util.function.Function;
+
+public class StringTransformation {
+  public static <R> R transformFirstOption(String string,
+      Function<? super String, ? extends R> function) {
+    return function.apply(string);
+  }
+
+  public static String transformSecondOption(String string,
+      Function<? super String, ? extends String> function) {
+    return string.transform(function);
+  }
+}
---
+++ src/test/java/field/string/StringTransformationTest.java
@@ -0,0 +1,35 @@
+package field.string;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
+
+class StringTransformationTest {
+  private static final Function<String, String> TO_UPPER_CASE = String::toUpperCase;
+  private static final String TEST_STRING = "Hello world";
+  private static final String EXPECTED_STRING = "HELLO WORLD";
+
+  /**
+   * Method under test: {@link StringTransformation#transformFirstOption(String, Function)}
+   */
+  @Test
+  void testTransformFirstOption() {
+    assertEquals(
+        EXPECTED_STRING,
+        StringTransformation.transformFirstOption(TEST_STRING, TO_UPPER_CASE)
+    );
+  }
+
+  /**
+   * Method under test: {@link StringTransformation#transformSecondOption(String, Function)}
+   */
+  @Test
+  void testTransformSecondOption() {
+    assertEquals(
+        EXPECTED_STRING,
+        StringTransformation.transformSecondOption(TEST_STRING, TO_UPPER_CASE)
+    );
+  }
+}
+
