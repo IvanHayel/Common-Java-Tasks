febe875aaa66fac4cf47b93bdb8f4fc68908bc85
Ivan Hayel <i.hayel.dev@gmail.com>
2022-07-17 10:28:04 +0300

    Using the var type with primitive types solved.

---
+++ src/main/java/field/lvti/LvtiWithPrimitives.java
@@ -0,0 +1,23 @@
+package field.lvti;
+
+public class LvtiWithPrimitives {
+  public static void main(String[] args) {
+    char charValue = 'a';
+    boolean booleanValue = true;
+    byte byteValue = 1;
+    short shortValue = 1;
+    int intValue = 1;
+    long longValue = 1;
+    float floatValue = 1.0f;
+    double doubleValue = 1.0;
+
+    var lvtiChar = 'a';
+    var lvtiBoolean = true;
+    var lvtiByte = (byte) 1;
+    var lvtiShort = (short) 1;
+    var lvtiInt = 1;
+    var lvtiLong = 1L;
+    var lvtiFloat = 1.0F;
+    var lvtiDouble = 1.0D;
+  }
+}
